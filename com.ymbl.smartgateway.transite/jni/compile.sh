4d1ada2c721e0bfc95cc516005792f3a783aba06
Sam Chen <xuejian1354@163.com>
2018-04-18 02:10:43 -0400

    Add iptables by jni

--- com.ymbl.smartgateway.transite/jni/compile.sh
+++ com.ymbl.smartgateway.transite/jni/compile.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-#CC=mips-unknown-linux-uclibc-gcc
-CC=gcc
-
 EXSH=`pwd`/$0
 EXDIR=$(dirname ${EXSH})
+
+PRETARGET=/tmp/transite-target
 
 cd ${EXDIR}
 
 [ ! -d "iptables" ] && \
-    git clone -b v1.6.2 git://git.netfilter.org/iptables && \
+    git clone -b v1.4.21 git://git.netfilter.org/iptables && \
     rm -rf iptables/.git && \
-    patch -p0 < iptables-jni-1.6.2.patch && \
+    patch -p0 < patches/iptables-jni-1.4.21.patch && \
     cp -v com_ymbl_smartgateway_transite_TransiteActivator.c iptables/iptables/ && \
     cp -v com_ymbl_smartgateway_transite_TransiteActivator.h iptables/iptables/ && \
     cp -v dlog.h iptables/iptables/
@@ -29,9 +28,17 @@ isnew=`find dlog.h -newer iptables/iptables/dlog.h`
     cp -v dlog.h iptables/iptables/
 
 cd iptables
+rm -rf ${PRETARGET}
 
 [ ! -x "configure" ] && \
-    ./autogen.sh && ./configure --prefix=${EXDIR}/iptables/target
+    ./autogen.sh && ./configure --prefix=${PRETARGET} --host=mips-en751221-linux-gnu
+#   ./autogen.sh && ./configure --prefix=${PRETARGET}
 
 make && make install
-cp target/lib/libtransite.so.0.1.0 ../../src/main/java/transite.so
+rm -rf ${EXDIR}/iptables/target
+mv ${PRETARGET} ${EXDIR}/iptables/target
+cp -v ${EXDIR}/iptables/target/lib/libtransite.so.0.1.0 ../../src/main/java/transite.so
+cp -v ${EXDIR}/iptables/target/lib/libip4tc.so.0 ../../src/main/java/
+cp -v ${EXDIR}/iptables/target/lib/libip6tc.so.0 ../../src/main/java/
+cp -v ${EXDIR}/iptables/target/lib/libxtables.so.10 ../../src/main/java/
+cp -av ${EXDIR}/iptables/target/lib/xtables ../../src/main/java/
--- com.ymbl.smartgateway.transite/src/main/java/com/ymbl/smartgateway/transite/TransiteActivator.java
+++ com.ymbl.smartgateway.transite/src/main/java/com/ymbl/smartgateway/transite/TransiteActivator.java
@@ -85,7 +85,7 @@ public class TransiteActivator extends AbstractActivator implements Runnable{
 				return;
 			}
 
-			loadLib("transite");
+			addLoadLibs();
 		} catch (MissingResourceException e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -100,6 +100,48 @@ public class TransiteActivator extends AbstractActivator implements Runnable{
 		addRedirectHostRule();
 
 		startSelectListen(redirectPort);
+	}
+
+	public void addLoadLibs() throws IOException {
+		String[] xtables_lib_list = {
+				"libip6t_DNAT.so", "libip6t_DNPT.so", "libip6t_HL.so", "libip6t_LOG.so", 
+				"libip6t_MASQUERADE.so", "libip6t_NETMAP.so", "libip6t_REDIRECT.so", 
+				"libip6t_REJECT.so", "libip6t_SNAT.so", "libip6t_SNPT.so", "libip6t_ah.so", 
+				"libip6t_dst.so", "libip6t_eui64.so", "libip6t_frag.so", "libip6t_hbh.so", 
+				"libip6t_hl.so", "libip6t_icmp6.so", "libip6t_ipv6header.so", "libip6t_mh.so", 
+				"libip6t_rt.so", "libipt_CLUSTERIP.so", "libipt_DNAT.so", "libipt_ECN.so", 
+				"libipt_LOG.so", "libipt_MASQUERADE.so", "libipt_MIRROR.so", "libipt_NETMAP.so",
+				"libipt_REDIRECT.so", "libipt_REJECT.so", "libipt_SAME.so", "libipt_SNAT.so", 
+				"libipt_TTL.so", "libipt_ULOG.so", "libipt_ah.so", "libipt_icmp.so", 
+				"libipt_realm.so", "libipt_ttl.so", "libipt_unclean.so", "libxt_AUDIT.so", 
+				"libxt_CHECKSUM.so", "libxt_CLASSIFY.so", "libxt_CONNMARK.so", 
+				"libxt_CONNSECMARK.so", "libxt_CT.so", "libxt_DSCP.so", "libxt_HMARK.so",
+				"libxt_IDLETIMER.so", "libxt_LED.so", "libxt_MARK.so", "libxt_NFLOG.so", 
+				"libxt_NFQUEUE.so", "libxt_NOTRACK.so", "libxt_RATEEST.so", "libxt_SECMARK.so",
+				"libxt_SET.so", "libxt_SYNPROXY.so", "libxt_TCPMSS.so", "libxt_TCPOPTSTRIP.so",
+				"libxt_TEE.so", "libxt_TOS.so", "libxt_TPROXY.so", "libxt_TRACE.so",
+				"libxt_addrtype.so", "libxt_bpf.so", "libxt_cluster.so", "libxt_comment.so", 
+				"libxt_connbytes.so", "libxt_connlimit.so", "libxt_connmark.so", 
+				"libxt_conntrack.so", "libxt_cpu.so", "libxt_dccp.so", "libxt_devgroup.so", 
+				"libxt_dscp.so", "libxt_ecn.so", "libxt_esp.so", "libxt_hashlimit.so", 
+				"libxt_helper.so", "libxt_iprange.so", "libxt_ipvs.so", "libxt_length.so",
+				"libxt_limit.so", "libxt_mac.so", "libxt_mark.so", "libxt_multiport.so",
+				"libxt_nfacct.so", "libxt_osf.so", "libxt_owner.so", "libxt_physdev.so", 
+				"libxt_pkttype.so", "libxt_policy.so", "libxt_quota.so", "libxt_rateest.so",
+				"libxt_recent.so", "libxt_rpfilter.so", "libxt_sctp.so", "libxt_set.so",
+				"libxt_socket.so", "libxt_standard.so", "libxt_state.so", "libxt_statistic.so",
+				"libxt_string.so", "libxt_tcp.so", "libxt_tcpmss.so", "libxt_time.so", 
+				"libxt_tos.so", "libxt_u32.so", "libxt_udp.so"
+		};
+
+		loadLib("libip4tc.so.0", "/tmp/transite-target/lib", false, false);
+		loadLib("libip6tc.so.0", "/tmp/transite-target/lib", false, false);
+		loadLib("libxtables.so.10", "/tmp/transite-target/lib", false, false);
+		for (String xtlib : xtables_lib_list) {
+			loadLib(xtlib, "/tmp/transite-target/lib/xtables", false, false);
+		}
+
+		loadLib("transite.so", "/tmp/transite-target/lib", true, loadreslib);
 	}
 
 	public void getMacFromJNITest() {
@@ -184,22 +226,32 @@ public class TransiteActivator extends AbstractActivator implements Runnable{
 		iptables("iptables -t nat -D PREROUTING 1");
 	}
 
-	private synchronized static void loadLib(String libName) throws IOException {
-		String systemType = System.getProperty("os.name");
-		String libExtension = (systemType.toLowerCase().indexOf("win")!=-1) ? ".dll" : ".so";  
-		String libFullName = libName + libExtension;
-		String nativeTempDir = System.getProperty("java.io.tmpdir");
+	private synchronized static void loadLib(String libName, String targetDir, boolean fornative, boolean isreload) throws IOException {
+
 		InputStream in = null;
 		BufferedInputStream reader = null;
 		FileOutputStream writer = null;  
 
-		File extractedLibFile = new File(nativeTempDir+File.separator+libFullName);
-		if(!extractedLibFile.exists() || loadreslib == true) {
+		File ftd = new File(targetDir);
+		if (ftd.isFile()) {
+			ftd.delete();
+		}
+
+		if (!ftd.exists()) {
+			ftd.mkdirs();
+		}
+
+		File extractedLibFile = new File(targetDir+"/"+libName);
+		if (isreload && extractedLibFile.exists()) {
+			extractedLibFile.delete();
+		}
+
+		if(!extractedLibFile.exists()) {
 			try {
-				in = TransiteActivator.class.getResourceAsStream("/" + libFullName);
+				in = TransiteActivator.class.getResourceAsStream("/" + libName);
 				if(in==null)
-					in =  TransiteActivator.class.getResourceAsStream(libFullName);
-				TransiteActivator.class.getResource(libFullName);
+					in =  TransiteActivator.class.getResourceAsStream(libName);
+				TransiteActivator.class.getResource(libName);
 				reader = new BufferedInputStream(in);
 				writer = new FileOutputStream(extractedLibFile);
 
@@ -217,7 +269,10 @@ public class TransiteActivator extends AbstractActivator implements Runnable{
 					writer.close();
 			}
         }
-		System.load(extractedLibFile.toString());
+
+		if (fornative) {
+			System.load(extractedLibFile.toString());			
+		}
 	}
 
 	public native byte[] getMacAddr(String dev);
